899e9fdabde1f785f79e2f50ff2097dc53603da2
Vinod Kumar <vinodkgupta.1996@gmail.com>
2016-03-15 21:10:14 +0530

    added the second comment in Fact.cpp

--- Tuna/Fact.cpp
+++ Tuna/Fact.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-/*this is second program for calculating the factorial of limited no of value*/
+/*this is first program for calculating the factorial of limited no of value*/
+/*This is another statement added to check "git checkout CommitName --file_name " is working or not.*/
+
 int Fact(int n)
 {
 	if(n=1 || n==0) return 1;
